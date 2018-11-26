7caf1e4ca556e821301c5e378125ac66ba98fdbb
wisdomtooth <cbtsao47@gmail.com>
2018-11-25 22:56:28 -0600

    add App.css

---
+++ practice5/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Home() {
+  return (
+    <React.Fragment>
+      <h1>Avengers Database</h1>
+      <h4>Danger! This is classified information!</h4>
+    </React.Fragment>
+  );
+}
+
+export default Home;
