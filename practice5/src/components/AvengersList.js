35115f7025d29a5abec5a1b6a368af892af428d5
wisdomtooth <cbtsao47@gmail.com>
2018-12-10 23:41:05 -0600

    change Link to use the history object

--- practice5/src/components/AvengersList.js
+++ practice5/src/components/AvengersList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // import { prependOnceListener } from "cluster";
-
 function AvengersList(props) {
   // console.log(props);
   return (
@@ -9,14 +8,20 @@ function AvengersList(props) {
       {props.avengers.map(avenger => (
         <div className="character-card" key={avenger.id}>
           <img src={avenger.thumbnail} alt={avenger.name} />
-          <Link to={`/avengers/${avenger.id}`}>
-            <h2>{avenger.name}</h2>
-          </Link>
+          <h2>
+            {/* using history object to do it instead of Link */}
+            <div onClick={() => routeToAvenger(props, avenger)}>
+              {avenger.name}
+            </div>
+          </h2>
           <p>({avenger.nickname})</p>
         </div>
       ))}
     </div>
   );
+}
+function routeToAvenger(props, avenger) {
+  props.history.push(`/avengers/${avenger.id}`);
 }
 
 export default AvengersList;
